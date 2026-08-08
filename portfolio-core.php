9872f0eaa58b11b76478de6a6010c6ef2df94578
karim-khfr <karim.khenifer@ac-grenoble.fr>
2026-08-08 16:36:44 +0200

    Gère les règles de réécriture du plugin

--- portfolio-core.php
+++ portfolio-core.php
@@ -15,3 +15,30 @@ if (! defined('ABSPATH')) {
 require_once __DIR__ . '/inc/post-types.php';
 require_once __DIR__ . '/inc/taxonomies.php';
 require_once __DIR__ . '/inc/fields.php';
+
+/**
+ * Actions exécutées à l’activation du plugin.
+ *
+ * @return void
+ */
+function portfolio_core_activate(): void
+{
+    portfolio_core_register_project_post_type();
+    portfolio_core_register_project_type_taxonomy();
+    portfolio_core_register_project_technology_taxonomy();
+
+    flush_rewrite_rules();
+}
+
+/**
+ * Actions exécutées à la désactivation du plugin.
+ *
+ * @return void
+ */
+function portfolio_core_deactivate(): void
+{
+    flush_rewrite_rules();
+}
+
+register_activation_hook(__FILE__, 'portfolio_core_activate');
+register_deactivation_hook(__FILE__, 'portfolio_core_deactivate');
